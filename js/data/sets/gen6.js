05106c06038b35e5df262ff72782fd9028845469
101NotF0und <741598651@qq.com>
2023-11-01 23:43:12 +0800

    EscalcV1.02

--- js/data/sets/gen6.js
+++ js/data/sets/gen6.js
@@ -1,2 +1,195 @@
 /* AUTOMATICALLY GENERATED FROM @smogon/sets, DO NOT EDIT! */
-var SETDEX_XY = {};
+var SETDEX_XY = {
+"Purrloin":
+        {"Team Aqua Grunt Petalburg Woods":{"level":11,"ability":"Unburden","item":"Normal Gem","nature":"Hardy","moves":["Fury Swipes","Fake Out"]}
+    },
+"Houndour":
+        {"Team Aqua Grunt Petalburg Woods":{"level":11,"ability":"Unnerve","nature":"Hardy","moves":["Ember","Smog"]}
+    },
+"Poochyena":
+        {"Team Aqua Grunt Petalburg Woods":{"level":11,"ability":"Tough Claws","nature":"Hardy","moves":["Bite","Fire Fang"]}
+    },
+"Onix":
+        {"Youngster Josh":{"level":16,"ability":"Sturdy","item":"Eviolite","nature":"Hardy","moves":["Rock Throw","Rock Tomb","Dragon Breath"]}
+    },
+"Kakuna":
+        {"Youngster Kakuna":{"level":16,"ability":"Shed Skin","nature":"Hardy","moves":["Bug Bite","Electroweb"]}
+    },
+"Munchlax":
+        {"Youngster Kakuna":{"level":16,"ability":"Cheek Pouch","item":"Sitrus Berry","nature":"Hardy","moves":["Lick","Tackle"]}
+    },
+"Beedrill":
+        {"Youngster Kakuna":{"level":16,"ability":"Adaptability","nature":"Hardy","moves":["Poison Sting","Fury Cutter"]}
+    },
+"Nosepass":
+        {"Leader Roxanne":{"level":15,"ability":"Sturdy","item":"Eviolite","nature":"Hardy","moves":["Sandstorm","Thunder Wave","Rock Tomb","Shock Wave"]}
+    },
+"Sudowoodo":
+        {"Leader Roxanne":{"level":15,"ability":"Sturdy","item":"Lum Berry","nature":"Hardy","moves":["Razor Leaf","Rock Tomb","Rest"]}
+    },
+"Magcargo":
+        {"Leader Roxanne":{"level":16,"ability":"Flame Body","item":"Sitrus Berry","nature":"Hardy","moves":["Flame Burst","Ancient Power","Yawn"]}
+    },
+"Relicanth":
+        {"Leader Roxanne":{"level":16,"ability":"Sturdy","item":"Rindo Berry","nature":"Hardy","moves":["Rock Throw","Water Pulse","Mud Shot","Rock Tomb"]}
+    },
+"Aerodactyl":
+        {"Leader Roxanne":{"level":16,"ability":"Unnerve","item":"Aerodactylite","nature":"Hardy","moves":["Aerial Ace","Bite","Rock Throw","Thunder Fang"]}
+    },
+"Aerodactyl-Mega":
+        {"Leader Roxanne":{"level":16,"ability":"Tough Claws","item":"Aerodactylite","nature":"Hardy","moves":["Aerial Ace","Bite","Rock Throw","Thunder Fang"]}
+    },
+"Liepard":
+        {"Team Aqua Grunt Rusturf Tunnel":{"level":19,"ability":"Unburden","item":"Normal Gem","nature":"Hardy","moves":["Fake Out","Snarl","Covet"]}
+    },
+"Mightyena":
+        {"Team Aqua Grunt Rusturf Tunnel":{"level":19,"ability":"Strong Jaw","item":"Sitrus Berry","nature":"Hardy","moves":["Bite","Thunder Fang","Fire Fang"]}
+    },
+"Raticate":
+        {"Team Aqua Grunt Rusturf Tunnel":{"level":19,"ability":"Hustle","item":"Muscle Band","nature":"Hardy","moves":["Bite","Me First"]},
+        "Poké Fan Isabel":{"level":28,"ability":"Guts","item":"Muscle Band","nature":"Hardy","moves":["Super Fang","Bite","Flame Wheel"]}
+    },
+"Diggersby":
+        {"Backpacker Graeme":{"level":20,"ability":"Huge Power","item":"Sitrus Berry","nature":"Hardy","moves":["Double Kick","Quick Attack","Bulldoze"]},
+        "Pokémon Breeder Lydia":{"level":30,"ability":"Cheek Pouch","item":"Sitrus Berry","nature":"Hardy","moves":["Facade","Bulldoze","Rock Slide"]}
+    },
+"Combusken":
+        {"Battle Girl Laura":{"level":22,"ability":"Speed Boost","item":"Sitrus Berry","nature":"Hardy","moves":["Aerial Ace","Double Kick","Flame Charge"]}
+    },
+"Machoke":
+        {"Black Belt Hideki":{"level":22,"ability":"Guts","item":"Eviolite","nature":"Hardy","moves":["Low Sweep","Knock Off","Bulldoze","Rock Tomb"]}
+    },
+"Gurdurr":
+        {"Black Belt Hideki":{"level":22,"ability":"Guts","item":"Flame Orb","nature":"Hardy","moves":["Mach Punch","Rock Tomb","Thunder Punch"]}
+    },
+"Primeape":
+        {"Battle Girl Tessa":{"level":22,"ability":"Anger Point","item":"Focus Band","nature":"Hardy","moves":["Brick Break","Fire Punch","Endeavor"]}
+    },
+"Poliwrath":
+        {"Battle Girl Tessa":{"level":22,"ability":"Water Absorb","item":"Sitrus Berry","nature":"Hardy","moves":["Brick Break","Bubble Beam","Bulldoze","Ice Punch"]}
+    },
+"Hitmonlee":
+        {"Leader Brawly":{"level":23,"ability":"Unburden","item":"Sitrus Berry","nature":"Hardy","moves":["Brick Break","Knock Off","Rock Slide","Poison Jab"]}
+    },
+"Pignite":
+        {"Leader Brawly":{"level":23,"ability":"Blaze","item":"Salac Berry","nature":"Hardy","moves":["Brick Break","Rock Tomb","Flame Charge","Fire Pledge"]}
+    },
+"Hariyama":
+        {"Leader Brawly":{"level":23,"ability":"Guts","item":"Flame Orb","nature":"Hardy","moves":["Force Palm","Smelling Salts","Fake Out","Poison Jab"]}
+    },
+"Heracross":
+        {"Leader Brawly":{"level":24,"ability":"Moxie","item":"Coba Berry","nature":"Hardy","moves":["Aerial Ace","Megahorn","Brick Break","Rock Slide"]}
+    },
+"Lucario":
+        {"Leader Brawly":{"level":24,"ability":"Iron Fist","item":"Expert Belt","nature":"Hardy","moves":["Brick Break","Ice Punch","Metal Claw","Aura Sphere"]}
+    },
+"Medicham":
+        {"Sailor Huey":{"level":26,"ability":"Pure Power","nature":"Hardy","moves":["Psycho Cut","Force Palm","Rock Tomb","Fake Out"]}
+    },
+"Bibarel":
+        {"Tuber Ricky":{"level":26,"ability":"Simple","item":"Sitrus Berry","nature":"Hardy","moves":["Aqua Jet","Super Fang","Swords Dance"]}
+    },
+"Vanillish":
+        {"Sailor Edmond":{"level":26,"ability":"Weak Armor","item":"Eviolite","nature":"Hardy","moves":["Icy Wind","Water Pulse","Mirror Shot"]}
+    },
+"Linoone":
+        {"Tuber Lola":{"level":26,"ability":"Reckless","nature":"Hardy","moves":["Take Down"]}
+    },
+"Nidorino":
+        {"Team Aqua Grunt Museum #1":{"level":28,"ability":"Poison Point","item":"Eviolite","nature":"Hardy","moves":["Poison Jab","Drill Run","Counter"]}
+    },
+"Golbat":
+        {"Team Aqua Grunt Museum #2":{"level":28,"ability":"Infiltrator","item":"Black Sludge","nature":"Hardy","moves":["Air Slash","Poison Fang"]}
+    },
+"Ariados":
+        {"Team Aqua Grunt Museum #2":{"level":28,"ability":"Sniper","item":"Scope Lens","nature":"Hardy","moves":["Cross Poison","X-Scissor"]}
+    },
+"Seviper":
+        {"Team Aqua Grunt Museum #2":{"level":28,"ability":"Shed Skin","item":"Sitrus Berry","nature":"Hardy","moves":["Poison Fang","Bite","Giga Drain"]}
+    },
+"Pikachu":
+        {"Poké Fan Isabel":{"level":28,"ability":"Static","item":"Shuca Berry","nature":"Hardy","moves":["Spark","Discharge","Brick Break","Grass Knot"]}
+    },
+"Eevee":
+        {"Poké Fan Isabel":{"level":28,"ability":"Adaptability","item":"Chople Berry","nature":"Hardy","moves":["Take Down","Baby-Doll Eyes","Swift"]}
+    },
+"Beautifly":
+        {"Pokémon Trainer May Route 110":{"level":30,"ability":"Adaptability","item":"Focus Sash","nature":"Hardy","moves":["Air Slash","Silver Wind","Giga Drain","Electroweb"]}
+    },
+"Wartortle":
+        {"Pokémon Trainer May Route 110":{"level":30,"ability":"Shell Armor","item":"Eviolite","nature":"Hardy","moves":["Aqua Tail","Fake Out","Icy Wind","Zen Headbutt"]}
+    },
+"Aromatisse":
+        {"Aroma Lady Daisy":{"level":28,"ability":"Aroma Veil","nature":"Hardy","moves":["Draining Kiss","Charge Beam"]}
+    },
+"Slurpuff":
+        {"Aroma Lady Daisy":{"level":28,"ability":"Sweet Veil","nature":"Hardy","moves":["Draining Kiss","Drain Punch"]}
+    },
+"Tranquill":
+        {"Twins Amy & Liv":{"level":28,"ability":"Super Luck","nature":"Hardy","moves":["Air Slash","Round"]}
+    },
+"Chatot":
+        {"Twins Amy & Liv":{"level":28,"ability":"Keen Eye","nature":"Hardy","moves":["Chatter","Round"]}
+    },
+"Delcatty":
+        {"Poké Fan Miguel":{"level":28,"ability":"Pixilate","item":"Wise Glasses","nature":"Hardy","moves":["Water Pulse","Fake Out","Round","Mud Bomb"]}
+    },
+"Minun":
+        {"Teammates Anna & Meg":{"level":30,"ability":"Minus","item":"Sitrus Berry","nature":"Hardy","moves":["Thunder Wave","Round","Helping Hand","Fake Tears"]}
+    },
+"Fearow":
+        {"Triathlete Dylan":{"level":30,"ability":"Sniper","item":"Flying Gem","nature":"Hardy","moves":["Pluck","Drill Run","Steel Wing"]}
+    },
+"Xatu":
+        {"Triathlete Dylan":{"level":30,"ability":"Synchronize","item":"Wise Glasses","nature":"Hardy","moves":["Ominous Wind","Air Slash","Giga Drain"]}
+    },
+"Marowak":
+        {"Pokémon Breeder Lydia":{"level":30,"ability":"Battle Armor","item":"Ground Gem","nature":"Hardy","moves":["Bone Rush","Fire Punch","Headbutt"]}
+    },
+"Piloswine":
+        {"Pokémon Breeder Lydia":{"level":30,"ability":"Thick Fat","item":"Muscle Band","nature":"Hardy","moves":["Ice Fang","Bulldoze","Ice Shard"]}
+    },
+"Ninjask":
+        {"Bug Maniac Derek":{"level":30,"ability":"Filter","item":"Charti Berry","nature":"Hardy","moves":["Bug Bite","Aerial Ace","Night Slash"]}
+    },
+"Volbeat":
+        {"Bug Maniac Derek":{"level":30,"ability":"Serene Grace","item":"Muscle Band","nature":"Hardy","moves":["Feint Attack","Leech Life","Zen Headbutt","Protect"]}
+    },
+"Drowzee":
+        {"Pokémon Breeder Isaac":{"level":30,"ability":"Insomnia","item":"Eviolite","nature":"Hardy","moves":["Psybeam","Reflect","Hypnosis","Brick Break"]}
+    },
+"Dugtrio":
+        {"Pokémon Trainer Wally Mauville City":{"level":31,"ability":"Arena Trap","item":"Muscle Band","nature":"Hardy","moves":["Brick Break","Bulldoze","Shadow Claw","Slash"]}
+    },
+"Magneton":
+        {"Pokémon Trainer Wally Mauville City":{"level":31,"ability":"Sturdy","item":"Wise Glasses","nature":"Hardy","moves":["Tri Attack","Thunderbolt","Mirror Shot","Thunder Wave"]}
+    },
+"Gallade":
+        {"Pokémon Trainer Wally Mauville City":{"level":32,"ability":"Super Luck","item":"Galladite","nature":"Hardy","moves":["Zen Headbutt","Ice Punch","Brick Break","Leaf Blade"]}
+    },
+"Gallade-Mega":
+        {"Pokémon Trainer Wally Mauville City":{"level":32,"ability":"Super Luck","item":"Galladite","nature":"Hardy","moves":["Zen Headbutt","Ice Punch","Brick Break","Leaf Blade"]}
+    },
+"Dedenne":
+        {"Guitarist Kirk":{"level":32,"ability":"Static","item":"Sitrus Berry","nature":"Hardy","moves":["Thunderbolt","Signal Beam","Play Rough","Thunder Wave"]}
+    },
+"Luxray":
+        {"Youngster Ben":{"level":32,"ability":"Guts","item":"Flame Orb","nature":"Hardy","moves":["Thunder Fang","Ice Fang","Fire Fang","Crunch"]}
+    },
+"Electabuzz":
+        {"Battle Girl Vivian":{"level":32,"ability":"Static","item":"Shuca Berry","nature":"Hardy","moves":["Thunder Punch","Ice Punch","Fire Punch","Brick Break"]}
+    },
+"Emolga":
+        {"Guitarist Shawn":{"level":32,"ability":"Motor Drive","item":"Wise Glasses","nature":"Hardy","moves":["Thunderbolt","Air Slash","Signal Beam","Volt Switch"]}
+    },
+"Lanturn":
+        {"Guitarist Shawn":{"level":32,"ability":"Water Absorb","item":"Shuca Berry","nature":"Hardy","moves":["Thunderbolt","Water Pulse","Icy Wind","Volt Switch"]}
+    },
+"Raichu":
+        {"Leader Wattson":{"level":33,"ability":"Static","item":"Air Balloon","nature":"Hardy","moves":["Surf","Discharge","Extrasensory","Grass Knot"]}
+    },
+"Ampharos":
+        {"Leader Wattson":{"level":33,"ability":"Static","item":"Shuca Berry","nature":"Hardy","moves":["Power Gem","Discharge","Dragon Pulse","Fire Punch"]}
+    }
+
+};
